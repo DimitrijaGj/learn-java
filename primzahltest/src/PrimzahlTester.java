fdea9b5b89660b5a7f06863bfd3f6c9fdc6e2096
Dimitrija Gjoshev <dimitrijagjoshev@Dimitrijas-MacBook-Pro-2.local>
2025-04-18 14:14:55 +0200

    My first Home Work

---
+++ primzahltest/src/PrimzahlTester.java
@@ -0,0 +1,20 @@
+public class PrimzahlTester {
+        /**
+         * prueft ob die uebergebene nichtnegative Zahl eine Primzahl ist
+         * wenn die uebergebene Zahl eine Primzahl ist, wird JA ausgegeben
+         * ansonsten NEIN
+         * Eine Primzahl ist eine Zahl, die nur durch 1 und sich selbst (ohne Rest) teilbar ist.
+         * @param n die zu testende nichtnegative Zahl
+         */
+        public void istPrimzahl(long n) {
+                n = 5;
+               // pruf ob zahl is durch sich selber teilbar ist ohne Rest
+                if 'n > 2 && n % n' == 0 {
+                        System.out.println("JA");
+                else 
+                        System.out.println("NEIN");
+                
+                } 
+
+
+        }
