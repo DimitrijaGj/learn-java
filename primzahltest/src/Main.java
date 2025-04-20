3d760d3b382b5d240255460a4d1638607a5eadc2
Dimitrija Gjoshev <dimitrijagjoshev@Dimitrijas-MacBook-Pro-2.local>
2025-04-20 20:55:08 +0200

    final

---
+++ primzahltest/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        PrimzahlTester tester = new PrimzahlTester();
+        tester.istPrimzahl(7); // Example: Test if 7 is a prime number
+    }
+}
--- primzahltest/src/PrimzahlTester.java
+++ primzahltest/src/PrimzahlTester.java
@@ -7,7 +7,6 @@ public class PrimzahlTester {
      * @param n die zu testende nichtnegative Zahl
      */
     public void istPrimzahl(long n) {
-        n = 5L;
        // pruf ob zahl is durch sich selber teilbar ist ohne Rest
         if (n > 2L && n % n == 0L) {
             System.out.println("JA");
@@ -16,10 +15,5 @@ public class PrimzahlTester {
             System.out.println("NEIN");
 
         }
-    }
-
-    public static void main(String[] args) {
-        PrimzahlTester tester = new PrimzahlTester();
-        tester.istPrimzahl(7); // Example: Test if 7 is a prime number
     }
 }
