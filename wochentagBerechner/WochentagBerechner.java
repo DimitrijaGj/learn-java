83e06b522f325227cda2a0634954b1f1a0c89a0d
Dimitrija Gjoshev <dimitrijagjoshev@Dimitrijas-MacBook-Pro-2.local>
2025-04-20 21:12:00 +0200

    started

--- wochentagBerechner/WochentagBerechner.java
+++ wochentagBerechner/WochentagBerechner.java
@@ -8,6 +8,24 @@ public class WochentagBerechner {
 	 * @param jahr die vierstellige Jahreszahl (z. B. 2009)
 	 */
 	public void berechneWochentag(int tag, int monat, int jahr) {
+		// Calculate the day
+		// 0 = Sonntag, 1 = Montag, 2 = Dienstag, 3 = Mittwoch, 4 = Donnerstag, 5 = Freitag, 6 = Samstag
+		// 0 = Sonntag, 1 = Montag, 2 = Dienstag, 3 = Mittwoch, 4 = Donnerstag, 5 = Freitag, 6 = Samstag	
+		int t = tag;
+		int m = monat;
+		int j = jahr;	
+		int c = j / 100; // Jahrhundert
+		int k = j % 100; // Jahr innerhalb des Jahrhunderts
+		if (m < 3) {
+			m += 12; // Januar und Februar als 13. und 14. Monat des Vorjahres
+			j--;
+		}
+		// Berechnung des Wochentags
+		// w = (t + (13 * (m + 1))/5 + k + (k/4) + (c/4) - 2*c) % 7;
+
+		int w;
+		w = (t + (26 * (m + 1))/10 + (5 * j)/4 + c/4 + 5 * c - 1) % 7;
+
 	}
 	
 	public static void main(String[] args) {
@@ -24,6 +42,7 @@ public class WochentagBerechner {
 		System.out.print(".");
 		System.out.print(j);
 		System.out.print(" ist ein ");
+		
 		new WochentagBerechner().berechneWochentag(t, m, j);
 	}
 
