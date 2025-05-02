9e2ed3a8939df3f9396e630ba754716a2b0e6086
Dimitrija Gjoshev <dimitrijagjoshev@Dimitrijas-MacBook-Pro-2.local>
2025-05-02 20:24:10 +0200

    started to put some valiues

--- durchschnitt/src/main/java/Durchschnitt.java
+++ durchschnitt/src/main/java/Durchschnitt.java
@@ -1,3 +1,5 @@
+
+
 /**
  * Programm zur Berechnung des Durschnittes aus einem Array von int-Zahlen.
  */
@@ -10,8 +12,20 @@ public class Durchschnitt {
 	 * @param feld ein Array von int-Zahlen.
 	 * @return Der berechnete Durchschnitt der Zahlen im Array.
 	 */
+	
+
 	public double berechneDurchschnitt(int[] feld) {
 		// TODO: Hier bitte die entsprechende Berechnung ergaenzen.
+		// Check ob Numer  nicht negativ isaddieren die zusammen
+		// Summe teilen durch Länge der Arrays
+		System.out.println("is this working");
 		return 0;
 	}
+	
+	public static void main(String[] args) {
+        Durchschnitt d = new Durchschnitt();
+        int[] testArray = {1, 2, 3, 4, 5};
+        double result = d.berechneDurchschnitt(testArray);
+        System.out.println("Durchschnitt: " + result);
+    }
 }
