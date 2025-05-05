705126b6f68551de0633f6f2e2f45080ecbd8d30
Dimitrija Gjoshev <dimitrijagjoshev@Dimitrijas-MacBook-Pro-2.local>
2025-05-05 19:15:14 +0200

    added test

---
+++ bank/bank/src/Konto.java
@@ -0,0 +1,14 @@
+public class Konto {
+/**
+* Geldbeträge werden in Cent angegeben. Das gewünschte Verhalten des Konstruktors und der Methoden ist im Folgenden beschrieben:
+* Konto(long kontoNr, Bank b, Kunde k): Der Konstruktor soll die Informationen über Kontonummer, zugehörige Bank und den Kontoinhaber speichern. Der Kontostand eines neuen Kontos soll 0 Euro betragen
+* void auszahlen(long cent): Hier soll der Kontostand um den angegebenen Betrag verringert werden. Sie müssen sich keine Gedanken um eine mögliche Überziehung machen.
+* void einzahlen(long cent): Hier soll der Kontostand um den angegebenen Betrag erhöht werden.
+* String liefereBankname(): Der Name der zugehörigen Bank soll zurückgeliefert werden.
+* long liefereBLZ(): Die Bankleitzahl der zugehörigen Bank soll zurückgeliefert werden.
+* String liefereInhabername(): Der Name des Kontoinhabers soll zurückgeliefert werden.
+* long liefereKontonummer(): Die Kontonummer soll zurückgeliefert werden.
+* long liefereKontostand(): Die aktuelle Kontostand (in Cent) soll zurückgeliefert werden.
+ */
+    
+}
