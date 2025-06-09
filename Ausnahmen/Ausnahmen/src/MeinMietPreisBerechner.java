dc5d9e5f06e8f5071546b2fe47ecdab7bff0bc1f
Dimitrija Gjoshev <dimitrijagjoshev@Dimitrijas-MacBook-Pro-2.local>
2025-06-09 16:50:48 +0200

    New Home work

---
+++ Ausnahmen/Ausnahmen/src/MeinMietPreisBerechner.java
@@ -0,0 +1,10 @@
+public class MeinMietPreisBerechner {
+    public long berechneMietpreis(int tage, int km) throws UngueltigeTagesanzahlAusnahme, UngueltigeKilometerangabeAusnahme {
+		int Mietdauer = tage;
+        int Kilometer = km;
+        int Pauschale = 0;
+        
+
+	}
+
+}
